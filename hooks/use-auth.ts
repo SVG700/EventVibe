561f4e79ade99623d73e71e3fb66019b4f6d563f
SVG700 <88542391+SVG700@users.noreply.github.com>
2026-01-04 16:16:59 +0530

    hooks

---
+++ hooks/use-auth.ts
@@ -0,0 +1,8 @@
+'use client';
+
+import { useUser } from '@/firebase/provider';
+
+export const useAuth = () => {
+    const { user, isUserLoading, userError } = useUser();
+    return { user, loading: isUserLoading, error: userError };
+};
